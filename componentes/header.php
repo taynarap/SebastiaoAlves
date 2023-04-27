067adec1a9934b4df322cf96e29a79bef1842af8
Taynara P <taynara.patelo@gmail.com>
2023-04-27 15:34:35 +0100

    Carousel em PHP final

--- componentes/header.php
+++ componentes/header.php
@@ -30,7 +30,7 @@
                             </a>
                             <ul class="dropdown-menu shadow">
                                 <li><a class="dropdown-item pb-3" href="senhora_amor_guerra.php">Senhora Do
-                                        Amor e DA Guerra</a>
+                                        Amor e Da Guerra</a>
                                 </li>
                                 <li><a class="dropdown-item pb-3" href="#">O Caracol Estrábico</a></li>
                                 <li><a class="dropdown-item pb-3" href="#">O Coleccionador de Amnésias</a>
@@ -57,76 +57,44 @@
 
     <div id="area-banner" class="carousel slide d-none d-sm-block" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#area-banner" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#area-banner" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#area-banner" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-            <button type="button" data-bs-target="#area-banner" data-bs-slide-to="3"
-                aria-label="Slide 4"></button>
+
+            <?php foreach($carousel as $chave => $banner): ?>
+
+                <?php if($chave == 0): ?>
+
+                <button type="button" data-bs-target="#area-banner" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+
+                <?php else: ?>
+               
+                    <button type="button" data-bs-target="#area-banner" data-bs-slide-to="<?= $chave; ?>"
+                    aria-label="Slide <?= intval($chave)+ 1 ;?>"></button>
+
+                <?php endif; ?>
+
+            <?php endforeach; ?>
         </div>
         <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="banner-imagem w-100" src="imgs/desktop/cabecalho1.jpg"
-                    alt="Livro Senhora do Amor e da Guerra">
-                <div class="carousel-caption d-none d-sm-block">
-                    <p class="banner-novidade">Novidade</p>
-                    <h5 class="banner-titulo">Senhora do Amor e da Guerra</h5>
-                    <p class="banner-texto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
-                        fuga excepturi consequatur, maxime eum sapiente a? Placeat, temporibus rem quas
-                        recusandae, sapiente voluptas fugiat dignissimos fuga modi numquam sunt, culpa ex! Natus
-                        dolor eveniet ducimus possimus mollitia nihil molestiae, porro dolore.</p>
-                    <div class="banner-sabermais">
-                        <button class="btn-sabermais"></button>
+
+            <?php foreach($carousel as $chave => $banner): ?>
+
+                <div class="carousel-item <?= ($chave == 0) ? "active" : ""; ?>">
+
+                    <img class="banner-imagem w-100" src="<?=$banner->imagem; ?>"
+                        alt="<?=$banner->titulo; ?>">
+                    <div class="carousel-caption d-none d-sm-block">
+                        <p class="banner-novidade"><?=$banner->novidade; ?></p>
+                        <h5 class="banner-titulo"><?=$banner->titulo; ?></h5>
+                        <p class="banner-texto"><?=$banner->texto; ?></p>
+                        <div class="banner-sabermais">
+                            <button class="btn-sabermais"></button>
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
-            <div class="carousel-item">
-                <img class="banner-imagem w-100" src="imgs/desktop/cabecalho2.jpg"
-                    alt="Livro O Caracol Estrábico">
-                <div class="carousel-caption d-none d-sm-block">
-                    <h5 class="banner-titulo">O Caracol Estrábico</h5>
-                    <p class="banner-texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ducimus
-                        ullam eos necessitatibus sequi aut enim accusamus! Quaerat vero eum quos eveniet nisi
-                        tempora non qui natus officiis! Perferendis voluptas autem dolorem voluptatum inventore
-                        aperiam cum cumque laborum.</p>
-                    <div class="banner-sabermais">
-                        <a href="Saber Mais - ">
-                            <button class="btn-sabermais"></button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img class="banner-imagem w-100" src="imgs/desktop/cabecalho3.jpg"
-                    alt="Livro O Coleccionador de Amnésias">
-                <div class="carousel-caption d-none d-sm-block">
-                    <h5 class="banner-titulo">O Coleccionador de Amnésias</h5>
-                    <p class="banner-texto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem
-                        molestias dolorum dolore veniam aspernatur, delectus commodi pariatur! Iure, maiores.
-                        Assumenda deleniti inventore esse in voluptatem ut veritatis? Fugiat reiciendis fugit
-                        dolore unde pariatur assumenda voluptatibus quas beatae illum, inventore!</p>
-                    <div class="banner-sabermais">
-                        <button class="btn-sabermais"></button>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img class="banner-imagem w-100" src="imgs/desktop/cabecalho4.jpg"
-                    alt="Livro O Velho que Pensava que Fugia">
-                <div class="carousel-caption d-none d-sm-block">
-                    <h5 class="banner-titulo">O Velho que Pensava que Fugia</h5>
-                    <p class="banner-texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-                        est cumque delectus, quis assumenda, labore quod architecto mollitia nobis molestias,
-                        dolorem laborum? Nobis debitis perspiciatis quibusdam doloremque, tempora autem.
-                        Laudantium, iste distinctio totam doloremque maiores nihil aspernatur unde vero?</p>
-                    <div class="banner-sabermais">
-                        <button class="btn-sabermais"></button>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
+            
         </div>
 
     </div>
