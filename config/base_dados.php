5120957c826e6703ce3a1c8cee4c7435018c3cb8
Taynara P <77408568+taynarap@users.noreply.github.com>
2023-05-08 13:07:01 +0100

    08/05/2023

--- config/base_dados.php
+++ config/base_dados.php
@@ -1,12 +1,5 @@
 <?php
 
-$base_dados = [
-    "dbname" => "sebastiaoalves_bd",
-    "host" => "localhost",
-    "user" => "root",
-    "password" => ""
-];
-
 $pdo = new PDO("mysql:dbname=".$base_dados["dbname"].";host=".$base_dados["host"], $base_dados["user"], $base_dados["password"]);
 
 function selectSQL($sql){
---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$base_dados = [
+    "dbname" => "sebastiaoalves_bd",
+    "host" => "localhost",
+    "user" => "root",
+    "password" => ""
+];
+
+?>
